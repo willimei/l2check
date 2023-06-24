1296d3e1743af0b68bd1a2c5290ac3a075d79374
Willi Meißner <willi.meissner@mailbox.tu-dresden.de>
2023-06-24 10:47:56 +0200

    introduce L2CheckSuite Class

--- main.py
+++ main.py
@@ -1,92 +1,84 @@
-import scapy.config
-
 from lib.readconfig import *
 from lib.interfaces import *
 from lib.connectivity import *
 from scapy.all import *
+import scapy.config
 import ipaddress
-import os
 from attacks.arp_cache_poison import ArpCachePoison
 from pyroute2 import netns as pyrouteNetns, IPRoute
 import netns
 
-Interfaces = []
-ip4network = ipaddress.ip_network('192.168.3.0/24')
+class L2CheckSuite:
+
+    def initialize(self, configfilename):
+        ipr = IPRoute()
+        interfaces = read_interfaces(configfilename)
+        for index, (ifname, ip) in enumerate(zip(interfaces, self.ip4network.hosts())):
+            namespace = f'host{index}'
+
+            pyrouteNetns.create(namespace)
+            ifindex = ipr.link_lookup(ifname=ifname)[0]
+            ipr.link('set', index=ifindex, net_ns_fd=namespace)
+
+            add_ip(ifname, ip, self.ip4network.prefixlen, namespace)
+            ifup(ifname, namespace)
+
+            with netns.NetNS(nsname=namespace):
+                scapy.config.conf.ifaces.reload()
+                scapy.config.conf.route.resync()
+                intf = dev_from_networkname(ifname)
+                self.Interfaces.append(intf)
+
+        ipr.close()
 
 
-def initialize(configfilename='config.txt'):
-    ipr = IPRoute()
-    interfaces = read_interfaces(configfilename)
-    for index, (ifname, ip) in enumerate(zip(interfaces, ip4network.hosts())):
-        namespace = f'host{index}'
+    def check_base_connectivity(self):
+        sniffer = []
+        for i in range(1,4):
+            filterstr = f'arp and ether dst {self.Interfaces[0].mac} and ether src {self.Interfaces[i].mac}'
+            sniffer.append(get_async_sniffer(self.Interfaces, i, filterstr))
 
-        pyrouteNetns.create(namespace)
-        ifindex = ipr.link_lookup(ifname=ifname)[0]
-        ipr.link('set', index=ifindex, net_ns_fd=namespace)
+        ans = arping_neighbors(self.Interfaces[0], 'host0', self.ip4network)
 
-        add_ip(ifname, ip, ip4network.prefixlen, namespace)
-        #all_ifup_netns(namespace)
-        ifup(ifname, namespace)
+        for s in sniffer:
+            s.stop()
 
-        with netns.NetNS(nsname=namespace):
-            scapy.config.conf.ifaces.reload()
-            scapy.config.conf.route.resync()
-            intf = dev_from_networkname(ifname)
-            Interfaces.append(intf)
+        ans_macs = []
+        for res in ans.res:
+            ans_macs.append(res.answer.src)
+        if (not ans_macs.__contains__(self.Interfaces[1].mac)) or \
+                (not ans_macs.__contains__(self.Interfaces[2].mac)):
+            raise SystemExit('Physical setup not valid. No ARP response.')
 
+        if (ans_macs.__contains__(self.Interfaces[3].mac)) and (len(sniffer[2].results.res) != 0):
+            raise SystemExit('Physical setup not valid. ARP response from isolated interface.')
 
-    ipr.close()
-
-    os.system('echo 1 > /proc/sys/net/ipv4/conf/all/arp_ignore')
-    os.system('echo 1 > /proc/sys/net/ipv4/conf/all/arp_filter')
-
-
-
-def check_base_connectivity():
-    sniffer = []
-    for i in range(1,4):
-        filterstr = f'arp and ether dst {Interfaces[0].mac} and ether src {Interfaces[i].mac}'
-        sniffer.append(get_async_sniffer(Interfaces, i, filterstr))
-
-    ans = arping_neighbors(Interfaces[0], 'host0', ip4network)
-
-    for s in sniffer:
-        s.stop()
-
-    ans_macs = []
-    for res in ans.res:
-        ans_macs.append(res.answer.src)
-    if (not ans_macs.__contains__(Interfaces[1].mac)) or \
-            (not ans_macs.__contains__(Interfaces[2].mac)):
-        raise SystemExit('Physical setup not valid. No ARP response.')
-
-    if (ans_macs.__contains__(Interfaces[3].mac)) and (len(sniffer[2].results.res) != 0):
-        raise SystemExit('Physical setup not valid. ARP response from isolated interface.')
-
-    for i in range(2):
-        if len(sniffer[i].results.res) != 1:
-            print(sniffer[i].results.res)
-            raise SystemExit('Physical setup not valid. ARP response from external Interface.')
+        for i in range(2):
+            if len(sniffer[i].results.res) != 1:
+                print(sniffer[i].results.res)
+                raise SystemExit('Physical setup not valid. ARP response from external Interface.')
 
 
+    def teardown(self):
+        for index, (i, ip) in enumerate(zip(self.Interfaces, self.ip4network.hosts())):
+            namespace = f'host{index}'
+            pyrouteNetns.remove(namespace)
 
 
-def teardown():
-    for index, (i, ip) in enumerate(zip(Interfaces, ip4network.hosts())):
-        namespace = f'host{index}'
-        pyrouteNetns.remove(namespace)
+    def __init__(self, configfile: str = 'config.txt'):
+        self.Interfaces = []
+        self.ip4network = ipaddress.ip_network('192.168.3.0/24')
+        self.initialize(configfile)
+        self.check_base_connectivity()
 
+    def __del__(self):
+        self.teardown()
 
 def main():
-    try:
-        initialize('config.txt')
-        check_base_connectivity()
-        poison = ArpCachePoison(Interfaces, ip4network)
-        result = poison.run()
-        print(f'Poison: {result}')
-    finally:
-        teardown()
-        pass
+    l2check = L2CheckSuite('config.txt')
+    poison = ArpCachePoison(l2check.Interfaces, l2check.ip4network)
+    result = poison.run()
+    print(f'Poison: {result}')
 
 
 if __name__ == '__main__':
