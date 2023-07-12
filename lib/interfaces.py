9b79060f729d0ccb7fcacbb677c181d3fec04ce4
Willi Meißner <willi.meissner@mailbox.tu-dresden.de>
2023-07-12 13:27:14 +0200

    use netns instead of pyroute2 for namespaces

--- lib/interfaces.py
+++ lib/interfaces.py
@@ -1,36 +1,34 @@
-import errno
-from pyroute2 import NDB, IPRoute, NetlinkError, netns
+from pyroute2 import NDB, IPRoute
 import ipaddress
+import netns
 
 
 def add_ip(ifname: str, ipaddress: ipaddress, prefixlen: str, namespace: str):
-    netns.pushns(namespace)
-    ipr = IPRoute()
-    ifindex = ipr.link_lookup(ifname=ifname)[0]
-    ipr.addr('add', ifindex, address=str(ipaddress), mask=prefixlen)
-    ipr.close()
-    netns.popns()
+    with netns.NetNS(nsname=namespace):
+        ipr = IPRoute()
+        ifindex = ipr.link_lookup(ifname=ifname)[0]
+        ipr.addr('add', ifindex, address=str(ipaddress), mask=prefixlen)
+        ipr.close()
 
 def all_ifup_netns(namespace: str):
-    netns.pushns(namespace)
-    with NDB(log='on') as ndb:
-        for key, nic in ndb.interfaces.items():
-            nic.set('state', 'up')
-            nic.commit()
-    netns.popns()
+    with netns.NetNS(nsname=namespace):
+        with NDB(log='on') as ndb:
+            for key, nic in ndb.interfaces.items():
+                nic.set('state', 'up')
+                nic.commit()
 
 def ifup(ifname: str, namespace: str):
-    netns.pushns(namespace)
-    ipr = IPRoute()
-    ifindex = ipr.link_lookup(ifname=ifname)[0]
-    ipr.link('set', index=ifindex, state='up')
-    ipr.poll(ipr.link_lookup, '', ifname=ifname, operstate='UP', timeout=20)
-    ipr.close()
-    netns.popns()
+    with netns.NetNS(nsname=namespace):
+        ipr = IPRoute()
+        ifindex = ipr.link_lookup(ifname=ifname)[0]
+        ipr.link('set', index=ifindex, state='up')
+        ipr.poll(ipr.link_lookup, '', ifname=ifname, operstate='UP', timeout=20)
+        ipr.close()
 
 
-def remove_ip(intf: str, ip: ipaddress, mask):
-    ipr = IPRoute()
-    ifindex = ipr.link_lookup(ifname=intf)
-    ipr.addr('delete', ifindex, address=str(ip), mask=mask)
-    ipr.close()
+def remove_ip(intf: str, ip: ipaddress, mask, namespace: str):
+    with netns.NetNS(nsname=namespace):
+        ipr = IPRoute()
+        ifindex = ipr.link_lookup(ifname=intf)
+        ipr.addr('delete', ifindex, address=str(ip), mask=mask)
+        ipr.close()
