40d3dbefe75e39953602740c251124d0b6d8fe0c
Willi Meißner <willi.meissner@mailbox.tu-dresden.de>
2023-07-12 18:06:32 +0200

    introduce abstract method for Attack class

--- attacks/attack.py
+++ attacks/attack.py
@@ -1,8 +1,16 @@
+import abc
 import scapy.all as scapy
 import ipaddress
 
-class Attack:
+class Attack(metaclass=abc.ABCMeta):
 
     def __init__(self, interfaces: list[scapy.NetworkInterface], ip4network: ipaddress.IPv4Network):
         self.interfaces = interfaces
         self.ip4network = ip4network
+
+    @abc.abstractmethod
+    def run(self) -> bool:
+        """
+        Run the attack.
+        :return: Boolean which indicates the success of the attack.
+        """
